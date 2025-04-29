7a33d770d64291adc7d54051f010087fee819483
Rusyaidi Azhari <rusyaidiazhari@Rusyaidis-Laptop.local>
2025-04-29 16:19:09 +0800

    add find by id, add delete

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/rushcode/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/rushcode/cruddemo/CruddemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 
 import com.rushcode.cruddemo.dao.AppDAO;
 import com.rushcode.cruddemo.entity.Instructor;
+import com.rushcode.cruddemo.entity.InstructorDetail;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -19,14 +20,58 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
 		return runner -> {
-			createInstructor(appDAO);
+			// createInstructor(appDAO);
+
+			// findInstructor(appDAO);
+
+			deleteInstructor(appDAO);
 		};
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+
+		int theId = 1;
+		System.out.println("Deleting instructor id: " + theId);
+
+		appDAO.deleteInstructorById(theId);
+
+		System.out.println("Done delete!");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+
+		int theId = 2;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associate instructorDetail only: " + tempInstructor.getInstructorDetail());
 	}
 
 	private void createInstructor(AppDAO appDAO) {
 
 		// create the instructor
-		Instructor tempInstructor = new Instructor(first)
+		// Instructor tempInstructor = new Instructor("John", "Cena", "john@gmail.com");
+
+		// InstructorDetail tempInstructorDetail = new
+		// InstructorDetail("http://www.rush.com", "Wrestling");
+
+		Instructor tempInstructor = new Instructor("Max", "Verstappen", "maxn@gmail.com");
+
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.max.com", "Racing");
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the instructor
+		//
+		// NOTE: this wil ALSO save the details object
+		// because of the CASCADE type ALL
+		System.out.println("Saving instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!");
 	}
 
 }
--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/rushcode/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/rushcode/cruddemo/dao/AppDAOImpl.java
@@ -1,12 +1,14 @@
 package com.rushcode.cruddemo.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.rushcode.cruddemo.entity.Instructor;
 
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 
+@Repository
 public class AppDAOImpl implements AppDAO {
 
     // define field for entity manager
@@ -22,6 +24,23 @@ public class AppDAOImpl implements AppDAO {
     @Transactional
     public void save(Instructor theInstructor) {
         entityManager.persist(theInstructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+
+        return entityManager.find(Instructor.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+
+        // retrieve the instructor
+        Instructor tempInstructor = entityManager.find(Instructor.class, theId);
+
+        // delete the instructor
+        entityManager.remove(tempInstructor);
     }
 
 }
